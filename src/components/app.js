9e165ff3253851f536a14469c7a632fb912d2498
vuday <vuday@sapient.com>
2017-07-18 16:42:07 +0530

    made a few changes to app.js and added a new file movielist.js

--- src/components/app.js
+++ src/components/app.js
@@ -2,14 +2,26 @@ import React, { Component } from 'react';
 
 export default class App extends Component {
 	
-	submithandler=(e)={
-		e.preventDefault();
-	}
+constructor(props){
+	super(props);
+	this.state={movie:''};
+}
+	
+submithandler=(e)={
+e.preventDefault();	
+//call action creator
+
+this.setState({movie:''});	
+}
+
+changehandler=(e)=>{
+	this.setState({movie:e.target.value});
+}
 	
   render() {
     return (
-	<form>
-      <input placeholder="enter the movie that you want to watch">
+	<form onSubmit={this.submithandler}>
+      <input value={this.state.movie} type="text" placeholder="enter the movie that you want to watch" onChange={this.changehandler}/>
 	  <button type="submit">Submit</button>
 	</form>
     );
